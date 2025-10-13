55313b3da04f645660a14ed2a94eb078d79586e1
Sandro Eduardo Fracaro <sandrof450@gmail.com>
2025-10-13 14:46:58 -0300

    feat: Adiciona ajuste do uri no arquivo .env e no consumer do rabbitmq 4

--- MicroServicoVendas/RabbitMQ/Consumers/RabbitMQConsumer.cs
+++ MicroServicoVendas/RabbitMQ/Consumers/RabbitMQConsumer.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
-using MicroServicoVendas.DTOs;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
